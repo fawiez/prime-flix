6ceb9dc97eb84ac703616c3ebd39cd88068de4b1
fabriciowiez <fabricio.ciechowiez@abase.com.br>
2025-02-04 17:14:35 -0300

    teste commit

---
+++ src/pages/Filme/index.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import './filme-info.css';
+
+function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "80502344f19924c3c7df4847491dd05c",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("FILME NÃO ENCONTRADO");
+            })
+        }
+        loadFilme();
+
+        return () => {
+            console.log("COMPONENTE NÃO ENCONTRADO");
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
+    return(
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>      
+            <h3>Sinopse</h3>  
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">
+                        Trailer
+                    </a>
+                </button>
+            </div>
+        </div>
+    );    
+}
+
+export default Filme;
---
+++ src/services/api.jsx
@@ -0,0 +1,9 @@
+// Base da URL: https://api.themoviedb.org/3/
+
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+});
+
+export default api;
